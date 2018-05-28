ee3f8b9263c994e54b5a0e8fa0a59a36a4e7182d
Gheorghe <iaconimgheorghe@gmail.com>
2018-05-28 11:45:01 +0300

    Create Homework_4.java

---
+++ Homework_4.java
@@ -0,0 +1,12 @@
+//Print the area and circumference of a circle from the radius (learn which types of variables to choose?)
+public class Homework_4{
+    public static void main(String[] args){
+
+        double pi = 3.14;
+        double radius = 10;
+        double circumference = 2*pi*radius;
+        double area = pi * radius * radius;
+
+        System.out.printf("Area of circle is: %f, and circumference: %f ",area, circumference);
+        }
+}
